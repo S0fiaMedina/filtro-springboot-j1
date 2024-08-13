7e61562152b8eccc111972ce06791a8bbf812e1b
S0fiaMedina <151967289+S0fiaMedina@users.noreply.github.com>
2024-08-13 06:48:09 -0500

    feat: :tada: initial commit

---
+++ src/main/java/com/filtrospringbootsofia/filtrospringboot/FiltrospringbootApplication.java
@@ -0,0 +1,13 @@
+package com.filtrospringbootsofia.filtrospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiltrospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiltrospringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filtrospringbootsofia/filtrospringboot/FiltrospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filtrospringbootsofia.filtrospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiltrospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
